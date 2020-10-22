540e1cc5b3e7267348231978483c9572ce758f7d
polina-tsvilodub <polina.tsvilodub@gmail.com>
2020-10-22 14:58:52 +0200

    add some plots

--- 06_main.js
+++ 06_main.js
@@ -26,13 +26,13 @@ $("document")
         ],
       // Here, you can specify all information for the deployment
       deploy: {
-        experimentID: "INSERT_A_NUMBER",
-        serverAppURL: "https://magpie-demo.herokuapp.com/api/submit_experiment/",
+        experimentID: "38",
+        serverAppURL: "https://pragmatics-ptb.herokuapp.com/api/submit_experiment/",
         // Possible deployment methods are:
         // "debug" and "directLink"
         // As well as "MTurk", "MTurkSandbox" and "Prolific"
         deployMethod: "debug",
-        contact_email: "YOUREMAIL@wherelifeisgreat.you",
+        contact_email: "ptsvilodub@uos.de",
         prolificURL: "https://app.prolific.ac/submissions/complete?cc=SAMPLE1234"
       },
       // Here, you can specify how the progress bar should look like
